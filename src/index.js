4034507434a34951ababd0c28e3b0e49c8d1dea4
Ihor Holysh <58191199+Ig0r0k@users.noreply.github.com>
2020-10-01 23:53:02 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length && array.length) {
+    return Math.min.apply(null, array);
+  } else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length && array.length) {
+    return Math.max.apply(null, array);
+  } else {
+    return 0;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length && array.length) {
+    let sum = array.reduce((a, b) => a + b, 0);
+    return sum / array.length;
+  } else {
+    return 0;
+  }
 }
